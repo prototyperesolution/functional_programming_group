6da01df5c6e4cab0eaef72687477a63daf2d6350
Bruno <brunojauad26@icloud.com>
2023-12-01 16:45:56 +0000

    added fighters for events

--- Types.hs
+++ Types.hs
@@ -21,9 +21,13 @@ data Fighter = Fighter {
     elo_rating :: Int
 } deriving (Show)
 
--- fighters have many to many relationship to fight dates
+-- fighters have many to many relationship to fight events
+
 type Date = (Int, Int, Int)
 data Event = Event {
-    fightDate :: [Date]
+    fightDate :: [Date],
+    fighterA :: Fighter,
+    fighterB :: Fighter
+
 } deriving (Show)
 
