1d375ab2fc3e8038929fb1f2469127123fd79307
edlowe16 <edlowe2001@gmail.com>
2023-12-04 23:16:12 +0000

    Add get function

--- app/Main.hs
+++ app/Main.hs
@@ -6,5 +6,12 @@ import Database
 main = do
     conn <- dbConnection
     let testFighter = Fighter "Big Steve" 100 50 1500
+    let testFighter2 = Fighter "John" 75 40 1200
     insertFighter conn testFighter
+    insertFighter conn testFighter2
+    maybeFighter <- getFighter conn "Big Steve"
+    case maybeFighter of
+        Just fighter -> putStrLn $ show fighter
+        Nothing -> putStrLn "Cannot find fighter"
     close conn
+
--- src/Database.hs
+++ src/Database.hs
@@ -4,8 +4,14 @@ module Database where
 
 
 import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
+import Database.SQLite.Simple.ToRow
 
-data Fighter = Fighter{name:: String, wins:: Int, losses:: Int, eloScore:: Int}
+
+data Fighter = Fighter{name:: String, wins:: Int, losses:: Int, eloScore:: Int} deriving (Show)
+
+instance FromRow Fighter where 
+    fromRow = Fighter <$> field <*> field <*> field <*> field
 
 dbConnection :: IO Connection
 dbConnection = do
@@ -18,3 +24,11 @@ insertFighter :: Connection -> Fighter -> IO()
 insertFighter conn fighter = do
     execute conn "INSERT INTO fighters (name, wins, losses, eloScore) VALUES (?, ?, ?, ?)"
         (name fighter, wins fighter, losses fighter, eloScore fighter)
+
+getFighter :: Connection -> String -> IO(Maybe Fighter)
+getFighter conn fighterName = do
+    queryedFighter <- query conn "SELECT * FROM fighters WHERE name = ?" (Only fighterName)
+    case queryedFighter of
+        [fighter] -> return (Just fighter)
+        _         -> return Nothing 
+
